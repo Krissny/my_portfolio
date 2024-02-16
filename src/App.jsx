85228dff7cda21be3dbfa6185326022e86ae218e
Krissny <110375406+Krissny@users.noreply.github.com>
2024-02-16 10:56:55 +0530

    Add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import styles from "./App.module.css";
+import { Hero } from "./components/hero/hero";
 import { Navbar } from "./components/navbar/navbar";
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
+      <Hero />
     </div>
   );
 }
